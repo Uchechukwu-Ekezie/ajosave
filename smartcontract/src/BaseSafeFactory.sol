2b7949c27c2a4e058359d4b5baed79ffbf71c3e9
Gbolahan Akande <geakande@gmail.com>
2025-12-14 07:08:46 +0100

    Import SafeERC20 library for safe transfers

--- smartcontract/src/BaseSafeFactory.sol
+++ smartcontract/src/BaseSafeFactory.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
@@ -16,6 +17,8 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
  */
 /* ========== ROTATIONAL POOL ========== */
 contract BaseSafeRotational is Ownable(msg.sender), ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
     address[] public members;
     mapping(address => bool) public hasDeposited;
     uint256 public totalMembers;
